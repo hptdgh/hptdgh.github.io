4edd958c18a7ea0c256d97a1cbbe50adbdc68143
hptdgh <57022306+hptdgh@users.noreply.github.com>
2023-06-15 15:01:30 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
--- Version:1.25
+-- Version:1.26
 local key = getSN();
 local sever = 'https://autofbios.app/api'
 function sleepWithToast(x,mess) -- nghỉ có hiện thông báo
@@ -368,7 +368,7 @@ function tuongtacIDPage(ID,slvuot, sllike,noidungcmt_tuongtacpage,tongsocmt_tuon
 		keoxuong(9000)
 		usleep(1000000)
 		if (like ~= sllike) then
-			local test1 = tapimg("likepostpage.jpg", 1, 1000000)
+			local test1 = tapimglike("likepostpage.jpg", 1, 1000000)
 			if (test1 == 1) then
 				like = like + 1
 			end
@@ -413,7 +413,7 @@ end
 function tapimglike(img, sl, time) -- tìm hình và click
     local img = findImage("/var/mobile/Library/AutoTouch/Scripts/facebook/img/"..img, sl, 0.99, nil)
     for i, v in pairs(img) do
-        local myTable = { '1', '2', '3'}
+        local myTable = { '1', '2'}
         local rancamxuc = tonumber(myTable[ math.random( #myTable ) ])
         if (rancamxuc == 1) then 
         tap(v[1], v[2])
